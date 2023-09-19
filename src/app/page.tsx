f2a90ea2b4b7b0aad96b9c541157894460f228f5
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-19 22:11:33 +0100

    add search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,31 @@ import Gallery from '@/components/gallery';
 import SearchBar from '@/components/search-bar';
 import { Fragment, useState } from 'react';
 import json from '@/data/images.json';
+import { TImage } from '@/types';
 
 export default function Home() {
   const [images, setImages] = useState(json);
+
+  const handleChange = (queryString: string) => {
+    // console.log('initial images', images);
+
+    const newImages = filterImages(json, queryString);
+    setImages(newImages);
+    // console.log('new images', newImages);
+  };
+
   return (
     <Fragment>
-      <SearchBar />
+      <SearchBar setImages={handleChange} />
       <Gallery images={images} setImages={setImages} />
     </Fragment>
+  );
+}
+
+function filterImages(prevState: TImage[], queryString: string) {
+  return prevState.filter((image) =>
+    image.tags.some((tag) =>
+      tag.title.toLowerCase().includes(queryString.toLowerCase())
+    )
   );
 }
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,4 +1,8 @@
-export default function SearchBar() {
+export default function SearchBar({
+  setImages,
+}: {
+  setImages: (e: any) => void;
+}) {
   return (
     <header className='py-6'>
       <div className='sm:max-w-xl mx-auto xs:max-w-xs px-4'>
@@ -9,6 +13,7 @@ export default function SearchBar() {
           id='search'
           placeholder='search by tag e.g. ball'
           className='px-6 py-2 border-gray-300 border w-full rounded-full focus-visible:outline-orange-400 hover:cursor-pointer'
+          onChange={(e) => setImages(e.target.value)}
         />
       </div>
     </header>
