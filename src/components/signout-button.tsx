ded1c8cdf08d41f839a15f0a04fb1f478de322f8
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-20 23:02:26 +0100

    minor fix to signout button

--- src/components/signout-button.tsx
+++ src/components/signout-button.tsx
@@ -4,7 +4,7 @@ import { signOut } from 'next-auth/react';
 export default function SignoutButton() {
   return (
     <button
-      className='bg-orange-400 text-white py-2 px-4 rounded-xl block'
+      className='bg-orange-400 hover:bg-orange-400/80 transition text-white py-2 px-4 rounded-xl block'
       onClick={() => signOut()}
     >
       sign out
